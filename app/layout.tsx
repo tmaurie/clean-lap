36593897a9e081edad099760e12c23da7e8cb3ce
tmaurie <thomas.maurier91@gmail.com>
2025-04-30 10:57:33 +0200

    Update metadata in layout and enhance footer with version info for improved clarity and SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,16 @@ import NextTopLoader from "nextjs-toploader";
 const geist = Geist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CleanLap",
-  description: "Une application pour les fans de Formule 1",
+  title: 'CleanLap',
+  description: 'Résultats et statistiques F1.',
+  openGraph: {
+    title: 'CleanLap',
+    description: 'Une application pour les fans de Formule 1. Votre tableau de bord F1 clair, rapide et accessible.',
+    url: 'https://cleanlap.vercel.app',
+    siteName: 'CleanLap',
+    locale: 'fr_FR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
