6c21e40d2ff69ffdd916a7bd0a370c1a80d2f253
Thomas Maurier <thomas.maurier91@gmail.com>
2026-08-11 15:10:58 +0200

    chore: update Next.js and ESLint dependencies; refactor ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTypescript,
   {
-    ignores: [".next/**", "node_modules/**"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/set-state-in-effect": "off",
+    },
   },
-];
+  globalIgnores([".next/**", "node_modules/**"]),
+]);
 
 export default eslintConfig;
